c215d537ad02dac8487dbbe4ef2117d8ec0cd989
Czempina <59874309+Czempina@users.noreply.github.com>
2021-02-25 15:03:16 +0100

    Add files via upload

---
+++ Controllers/StationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArvatoInventory.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArvatoInventory.Controllers
+{
+    public class StationController : Controller
+    {
+            private IStationRepository repository;
+            public StationController(IStationRepository repo)
+            {
+                repository = repo;
+            }
+        public ViewResult List() => View(repository.Stations);
+    }
+}
